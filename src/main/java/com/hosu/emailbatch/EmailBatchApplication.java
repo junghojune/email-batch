e7430f84fd678d924b17e300fd8252276ae3685f
hosu <76641246+junghojune@users.noreply.github.com>
2025-04-10 17:46:50 +0900

    feat : 초기 세팅

---
+++ src/main/java/com/hosu/emailbatch/EmailBatchApplication.java
@@ -0,0 +1,13 @@
+package com.hosu.emailbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hosu/emailbatch/EmailBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hosu.emailbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
